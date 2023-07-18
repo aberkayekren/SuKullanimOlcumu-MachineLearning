95939e29a63f4f60bd4babd9b64a5dd6abcf1078
Andaç Berkay Ekren <81115543+aberkayekren@users.noreply.github.com>
2023-07-18 13:29:25 +0300

    I imported the Epoch.py file into the neuron.py file

--- neuron.py
+++ neuron.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
+import epoch
 
 values = printData.values
 values = [float(value) for value in values]
